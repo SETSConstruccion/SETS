55f97d6485d528b576ba7ad1e64bb92819057e0c
Fernando Hernandez <fernando1999jw@gmail.com>
2020-11-22 09:53:02 -0600

    Ultima Fernando

--- Models/Administrador.cs
+++ Models/Administrador.cs
@@ -5,16 +5,14 @@ using System.Threading.Tasks;
 
 namespace SETS.Models
 {
-    public class Home
+    public class Administrador
     {
         public string Id { get; set; }
-        public string Correo{ get; set; }
-        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public int nombre { get; set; }
         public string IdTipoUsuario { get; set; }
         public string NombreUsuario { get; set; }
         public int Edad { get; set; }
         public string Password { get; set; }
-        
-
     }
 }
---
+++ Models/UsuarioUno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SETS.Models
+{
+    public class UsuarioUno
+    {
+        public string Id { get; set; }
+        public string nombre { get; set; }
+        public int nombreUsuario { get; set; }
+        public string password { get; set; }
+
+}
+}
+
